5db04678602ae9e1e18a04e56d3a36536b7662a5
amandonev <amandonev@icloud.com>
2021-11-14 20:44:06 +0900

    id不一致によるアクセス制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,31 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item_find, only: [:show, :edit, :update]
+
   def index
     @item = Item.all.order(id: 'DESC')
   end
 
   def new
     @item = Item.new
+  end
+
+  def show
+  end
+
+  def edit
+    if current_user.id == @item.user_id
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id), method: :get
+    else
+      render :edit
+    end
   end
 
   def create
@@ -22,5 +42,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :image, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item_find
+    @item = Item.find(params[:id])
   end
 end
